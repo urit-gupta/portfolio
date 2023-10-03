23f46b385d4d92adc82ddb19a99b36d9a6c4d483
Urit "Aaron" Gupta <uritgupta@gmail.com>
2023-10-02 21:00:27 -0300

    bug fix v2
    updated calculation of offset. now uses canvSize - windowHeight

--- squaresOfLight.js
+++ squaresOfLight.js
@@ -119,7 +119,8 @@ function parallax() {
   let pageHeight = max(body.scrollHeight, body.offsetHeight, html_.clientHeight, html_.scrollHeight, html_.offsetHeight);
   
   let { scrollY } = window;
-  origin.y = (scrollY / (max(pageHeight - windowHeight, 1))) * windowHeight * -0.5 * (1-(windowHeight / canvSize));
+  origin.y = (scrollY / (max(pageHeight - windowHeight, 1))) * -0.5 * (canvSize - windowHeight);
+  // canvSize * 0.5 * (1-(windowHeight / canvSize));
   // print(scrollY, (scrollY / (max(pageHeight - windowHeight, 1))), origin.y, 0.5 * (1-(windowHeight / canvSize)), pageHeight, windowHeight, canvSize);
 }
 
